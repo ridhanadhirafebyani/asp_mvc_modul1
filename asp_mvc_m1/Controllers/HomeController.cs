ff59b35b1497a381cc23607f1c223a5e40251958
ridhanadhirafebyani <ridha_febyani_24rpl@student.smktelkom-mlg.sch.id>
2017-02-14 14:27:36 -0800

    Add project files.

---
+++ asp_mvc_m1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace asp_mvc_m1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
---
+++ asp_mvc_m1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asp_mvc_m1.MvcApplication" Language="C#" %>
